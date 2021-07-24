7ae9a4e237fceb6575027b7f00680ac5a0f92c44
darthinvader <psaradakisj@gmail.com>
2021-07-24 14:15:52 +0100

    [init] js files to prettier style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import React from "react";
+
 class App extends Component {
   render() {
-    return <div className>{"helelo"}</div>;
+    return <div className></div>;
   }
 }
 
